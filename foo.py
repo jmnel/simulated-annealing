f1c9fcef7f83702f457ed5a8e2be56630e33eacb
Jacques Nel <jmnel92@gmail.com>
2020-11-14 18:40:57 -0500

    Init cooling schedule done.

--- foo.py
+++ foo.py
@@ -1,4 +1,5 @@
 from pprint import pprint
+from typing import Callable
 
 import matplotlib
 matplotlib.use('module://matplotlib-backend-kitty')
@@ -8,19 +9,13 @@ import matplotlib.patches as patches
 import numpy as np
 
 
-def f_rosenbrock(x, y):
+def rosenbrock(x, y):
     a, b = 1, 100
     return (a - x)**2 + b * (y - x**2)**2
 
 
-n_samples = 1000
-xlim = [-2, 2]
-ylim = [-1, 3]
-x, y = np.linspace(*xlim, n_samples), np.linspace(*ylim, n_samples)
-mx, my = np.meshgrid(x, y)
-z = np.power(f_rosenbrock(mx, my), 0.1)
 # z = f_rosenbrock(mx, my)
-plt.contourf(x, y, z, levels=100, cmap='viridis')
+# plt.contourf(x, y, z, levels=100, cmap='viridis')
 
 
 def gen_point(dom):
@@ -28,40 +23,52 @@ def gen_point(dom):
     return np.random.random(2) * (dom[:, 1] - dom[:, 0]) + dom[:, 0]
 #    return np.random.random(2)
 
-
-def sa(f):
-    dom = np.array([[-2, 2], [-1, 3]])
-    m = 1000
-    init_trials = [gen_point(dom) for _ in range(m + 1)]
-    f_m = np.diff([f(*p) for p in init_trials])
-
-    m_1 = sum(e <= 0 for e in f_m)
-    m_2 = m - m_1
-#    print(m_1)
-    acceptance_ratio = 0.9
-
-#    print([e for e in f_m if e > 0])
-    den = m_2 * acceptance_ratio + (1. - acceptance_ratio) * m_1
-    fbar = np.mean([v for v in f_m if v > 0])
-    print(f'fbar: {fbar}')
-    print(f'den: {den}')
-    print(f'm_1: {m_1}, m_2: {m_2}')
-    q = np.log(m_2 / den)
-    print(f'q: {q}')
-#    c_0 = np.mean([e for e in f_m if e > 0]) * (1 / (np.log(m_2 / (m_2 * xhi + (1 - xhi) * m_1))))
-#    print(c_0)
-
-#    pprint(f_m)
-#    f_m = np.diff(list(map(f, init_trials)))
-
-#    pprint(f_m)
-
-#    for idx, p in enumerate(init_trials):
-#        print(f'{idx} -> {p}')
-#    pass
+# def local_search(f,
 
 
-sa(f_rosenbrock)
+# def f(x, y):
+#    return -x**2 + -y**2
+
+def gen_point_a(dom: np.ndarray):
+    #    p = np.random.uniform(size=2) * dom[:, 1] - dom[:, 0] + dom[:, 0]
+    p = np.random.uniform(size=2) * (dom[1] - dom[0]) + dom[0]
+    return p
 
 
-# plt.show()
+def init_schedule(f: Callable,
+                  dom: np.ndarray,
+                  acceptance_ratio: float,
+                  m_trials: int):
+
+    #    f_plus = list()
+    chi = acceptance_ratio = 0.9
+
+    f_delta = [f(*gen_point_a(dom)) - f(*gen_point_a(dom)) for _ in range(m_trials)]
+    f_delta_plus = [e for e in f_delta if e > 0]
+    f_delta_plus = np.array(f_delta_plus)
+
+#    c = 2000
+#    plt.hist(np.exp(-f_delta_plus / c), bins=200)
+#    plt.show()
+#    m_2 = len(f_delta_plus)
+
+    cutoff = np.quantile(f_delta_plus, acceptance_ratio)
+    c0 = -cutoff / np.log(acceptance_ratio)
+
+    return c0
+
+
+def update_schedule(
+
+
+dom=np.array([[-2, -1], [2, 3]])
+f=rosenbrock
+# n_samples = 200
+# xlim = [-2, 2]
+# ylim = [-1, 3]
+# x, y = np.linspace(*xlim, n_samples), np.linspace(*ylim, n_samples)
+# mx, my = np.meshgrid(x, y)
+# z = np.power(rosenbrock(mx, my), 0.1)
+
+init_schedule(f, dom, acceptance_ratio=0.9, m_trials=100000)
+# sa(f)
