a03218461631ae2a5fbe193a80b019e91fbaa362
Jacques Nel <jmnel92@gmail.com>
2020-11-18 02:48:58 -0500

    Fixing issue with sigma=0.

--- sa.py
+++ sa.py
@@ -29,6 +29,13 @@ def goldstein_price(x):
                 * (18. - 32. * x1 + 12. * x1**2 + 48. * x2 - 36. * x1 * x2 + 27. * x2**2)))
 
 
+def grad_approx(f, x, tau=1e-14):
+    x1, x2 = x
+    return np.array([
+        (f([x1 + 0.5 * tau, x2]) - f([x1 - 0.5 * tau, x2])) / tau,
+        (f([x1, x2 + 0.5 * tau]) - f([x1, x2 - 0.5 * tau])) / tau])
+
+
 # def gen_point(dom):
 #    return np.random.random(2) * (dom[:, 1] - dom[:, 0]) + dom[:, 0]
 
@@ -60,10 +67,11 @@ hist = list()
 DOM_DIM = 2
 L0 = 20
 L = DOM_DIM * L0
-DELTA = 0.1
+DELTA = 0.2
 EPS = 1e-4
 CHI = 0.9
-SMOOTHING = 0.01
+# SMOOTHING = 1e-1
+SMOOTHING = 1.
 
 
 def fmin(f, dom):
@@ -86,6 +94,8 @@ def fmin(f, dom):
         for i in range(L):
             y = gen_point_a(dom)
 
+#            print(f'{n}:{i} -> c={c}, {np.exp(-(f(y)-f(x))/c)}')
+
             # Accept new point if it is downhill.
             if f(y) - f(x) <= 0.:
                 hist_inner.append(y)
@@ -103,8 +113,8 @@ def fmin(f, dom):
 
             f_at_c.append(f(x))
 
-        print(f'{n} -> m1={m_1}, m2={m_2}')
-        print(f'c={c}')
+#        print(f'{n} -> m1={m_1}, m2={m_2}')
+#        print(f'c={c}')
 
         if len(hist_inner) > 0:
             hist.append(hist_inner)
@@ -122,8 +132,10 @@ def fmin(f, dom):
 
         if len(f_at_c) > 0:
             sigma = np.std(f_at_c)
+#            if sigma == 0.:
+#                sigma = 1e-14
             c_old = c
-#            print(f'here: {sigma}, len of chain: {len(f_at_c)}')
+#            print(f'{n} -> c={c}, sigma={sigma}, len of chain: {len(f_at_c)}')
             c = c / (1. + (c * np.log(1. + DELTA)) / (3. * sigma))
 
         dc = c - c_old
@@ -165,30 +177,30 @@ def fmin(f, dom):
     return x
 
 
-#dom = np.array([[-2, -1], [2, 3]])
-#f = rosenbrock
+dom = np.array([[-2, -1], [2, 3]])
+f = rosenbrock
 
-f = goldstein_price
-dom = np.array([[-2, -2], [2, 2]])
+#f = goldstein_price
+#dom = np.array([[-2, -2], [2, 2]])
 
 res = fmin(f, dom)
 print(f'solution x={res}')
-#n_samples = 200
-#xlim = [-2, 2]
-#ylim = [-1, 3]
-#x, y = np.linspace(*xlim, n_samples), np.linspace(*ylim, n_samples)
-#mx, my = np.meshgrid(x, y)
-#z = np.power(rosenbrock([mx, my]), 0.1)
+# n_samples = 200
+# xlim = [-2, 2]
+# ylim = [-1, 3]
+# x, y = np.linspace(*xlim, n_samples), np.linspace(*ylim, n_samples)
+# mx, my = np.meshgrid(x, y)
+# z = np.power(rosenbrock([mx, my]), 0.1)
 
-#fig, ax = plt.subplots(1, 1)
-#ax.contourf(x, y, z)
+# fig, ax = plt.subplots(1, 1)
+# ax.contourf(x, y, z)
 
-#h_x = [h[-1][0] for h in hist]
-#h_y = [h[-1][1] for h in hist]
+# h_x = [h[-1][0] for h in hist]
+# h_y = [h[-1][1] for h in hist]
 
-#s_x = h_x[-1:]
-#s_y = h_y[-1:]
-#ax.scatter(s_x, s_y, c='red')
+# s_x = h_x[-1:]
+# s_y = h_y[-1:]
+# ax.scatter(s_x, s_y, c='red')
 
 # plt.show()
 
