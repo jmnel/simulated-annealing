4c73f88178379e97d2d857aad2788d132c48ce81
Gian Alix <gian.alix@gmail.com>
2020-11-22 19:53:30 -0500

    Updated multistart.py

--- multistart.py
+++ multistart.py
@@ -2,13 +2,13 @@
 import random as rnd
 import numpy as np
 
-def multi_start(n, a, b, K, f, tol):
+def multi_start(n, dom, K, f, tol):
     i = 1
     x_stars = [] # will hold all x_stars
     f_xstars = [] # will hold all values of f(x_star), where x_star is in x_stars
 
     while(i <= K):
-        x = np.random.uniform(a, b, n)
+        x = np.random.uniform(size=n)*(dom[1]-dom[0]) + dom[0]
         x_star = grad_descent(f, x, grad=None, tol=1e-14, max_iterations=10000, ls_method='exact', eps=1e-14, method='steepest_descent')
         x_stars.append(x_star)
         f_xstars.append(f(x_star))
